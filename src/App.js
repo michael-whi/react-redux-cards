8e96d317ff45126813016e5ea0e908edb9a2b241
michael-whi <michael_tugushev@mail.ru>
2019-09-09 18:45:44 +0300

    React-redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
 
 function App() {
   return (
