e92b1cef0215760b47de2cad3c4c53ad17b27ca7
michael-whi <michael_tugushev@mail.ru>
2019-09-14 22:32:00 +0300

    Redux preparing

---
+++ src/actions/index.js
@@ -0,0 +1,7 @@
+export function savePosition(state) {
+    return { type: SAVE_POS, state };
+}
+
+export function saveText(state) {
+    return { type: SAVE_TEXT, state };
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+export function mainReducer(state={}, action) => {
+    switch(action.type){
+        case "SAVE_POS":
+            return null
+        case "SAVE_TEXT":
+            return null    
+        default:
+            return state
+    }
+}
